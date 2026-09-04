73176b6dbbd50325ad93198bcb09fa805190499c
zakhyzulfandi-coder <zakhyzulfandi@gmail.com>
2026-09-05 04:27:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 window.AHIS_CONFIG = {
   APP_NAME: "SISFO AHIS",
   SCHOOL_NAME: "MTs Abu Hurairah",
-  APP_URL: "https://script.google.com/macros/s/AKfycbz2_jBBAsueDjoZMJ_qM5mFa_o0SQzHYrc2z-7xgvS7o2u8zr4EezFLYuhkEUc20cY3/exec",
+  APP_URL: "https://script.google.com/macros/s/AKfycbyjPmUMzLzWMWw6IRrZpagfhs4yiWmhDsoWmNbxqPE95m12WuCDTQxFiW_HdQNoKEwY6Q/exec",
   LOGO_URL: "icons/icon-192.png",
   LOGO_ABSOLUTE_URL: "https://zakhyzulfandi-coder.github.io/abuhurairah/icons/icon-192.png",
   THEME_COLOR: "#ffffff"
